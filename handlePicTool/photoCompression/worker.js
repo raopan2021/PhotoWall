324f71a071294e9da17d234b56448a23565a874d
raopan <raopan2021@outlook.com>
2025-07-29 20:18:24 +0800

    图片处理调整

--- handlePicTool/photoCompression/worker.js
+++ handlePicTool/photoCompression/worker.js
@@ -1,5 +1,4 @@
 import { promises } from "node:fs";
-// utils/image-worker.js
 import { extname, join } from "node:path";
 import sharp from "sharp";
 
@@ -14,7 +13,7 @@ import sharp from "sharp";
 export default async function processImage(task) {
   const { file, taskType, originDir, outputDir } = task;
   const inputPath = join(originDir, file);
-  const outputPath = join(outputDir, file);
+  const outputPath = join(outputDir, file.replace(/\.png|\.jpg|\.jpeg|\.webp$/i, ".webp"));// 替换为webp格式
 
   try {
     // 检查输出文件是否已存在
@@ -28,29 +27,22 @@ export default async function processImage(task) {
 
     if (taskType === "min") {
       // 小尺寸压缩 (宽度320px)
-      await sharp(inputPath).resize({ width: 320 }).toFile(outputPath);
+      await sharp(inputPath).resize({ width: 320 }).webp({ quality: 80 }).toFile(outputPath);
     }
     else {
       // 中等质量压缩 (保持原尺寸)
-      const fileExt = extname(file).toLowerCase();
-      const processor = sharp(inputPath);
+      const fileExt = extname(file).toLowerCase(); // 文件扩展名
+      const processor = sharp(inputPath); // sharp处理对象
 
       switch (fileExt) {
         case ".png":
-          await processor
-            .png({ quality: 80, compressionLevel: 9 })
-            .toFile(outputPath);
-          break;
         case ".jpg":
         case ".jpeg":
-          await processor
-            .jpeg({ quality: 80, mozjpeg: true })
-            .toFile(outputPath);
-          break;
         case ".webp":
           await processor.webp({ quality: 80 }).toFile(outputPath);
           break;
         default:
+          console.log(`转为webp格式失败，文件扩展名不正确: ${fileExt}`);
           await processor.toFile(outputPath);
       }
     }
